b5a2c18f46eb6f595ba023006c8900a66f4fe1d9
Praval <p.singhx26@gmail.com>
2024-04-06 22:12:29 +0530

    send the news through email

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import requests
-# import
+import smtplib, ssl
+import os
+from send_email import send_email
 
-url = ("https://newsapi.org/v2/everything?q=tesla&from=2024-03-05&"
+topic = "tesla"
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&from=2024-03-05&"
        "sortBy=publishedAt&apiKey="
-       "d395f29510bf401bb6e65148ae903558")
+       "d395f29510bf401bb6e65148ae903558&language=en")
 
 # Get API
 request = requests.get(url)
-# Connvert API into a python dictionary (JSON)
+# Convert API into a python dictionary (JSON)
 content = request.json()
+message = "Subject: Today's News \n"
 
-for article in content["articles"]:
-    print(article["title"])
+for article in content['articles'][:20]:
+    if article["title"] is not None and article["description"] is not None:
+        message = message + article["title"] + "\n" \
+                   + article["description"] + "\n" \
+                   + article["url"] + 2*"\n"
+
+message = message.encode("utf-8")
+send_email(message)
---
+++ send_email.py
@@ -0,0 +1,19 @@
+import smtplib, ssl
+import os
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "p.singhx26@gmail.com"
+    password = os.getenv("PASSWORD")
+
+    receiver = "p.singhx26@gmail.com"
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
